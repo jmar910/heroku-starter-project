29407c6cdc54e7b4f22c2ed8454e1b5442a9e2e6
James Martinez <jmar910@gmail.com>
2015-08-13 17:50:03 -0400

    Filter-link component tests

--- tests/integration/components/filter-link-test.js
+++ tests/integration/components/filter-link-test.js
@@ -24,3 +24,52 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), 'template block text');
 });
+
+test('clicking on the component', function(assert) {
+  assert.expect(2);
+
+  let allType = 'all';
+
+  this.set('type', allType);
+  this.set('currentFilterType', allType);
+
+  this.render(hbs`
+    {{#filter-link type=type currentFilterType=currentFilterType theAction='assertAction'}}
+      SHOW ALL
+    {{/filter-link}}
+  `);
+
+  this.on('assertAction', function(type) {
+    assert.ok(true);
+    
+    assert.equal(type, allType);
+  });
+
+  this.$('.filter-link').click();
+
+});
+
+test('active class is added', function(assert) {
+  assert.expect(2);
+
+  let allType = 'all';
+  let betaType = 'beta';
+
+  this.set('type', allType);
+  this.set('currentFilterType', betaType);
+
+  this.render(hbs`
+    {{#filter-link type=type currentFilterType=currentFilterType theAction='assertAction'}}
+      SHOW ALL
+    {{/filter-link}}
+  `);
+
+  let $component = this.$('.filter-link');
+
+  assert.ok(!$component.hasClass('active'));
+
+  this.set('currentFilterType', allType);
+
+  assert.ok($component.hasClass('active'));
+
+});
