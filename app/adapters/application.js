cf1b8575f1d703549f68c1bf3a87e0effb3ded87
James Martinez <jmar910@gmail.com>
2015-08-13 12:47:33 -0400

    var => let

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,9 +2,9 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   host: 'https://api.heroku.com',
-  
+
   pathForType(modelName) {
-    var dasherized = Ember.String.dasherize(modelName);
+    let dasherized = Ember.String.dasherize(modelName);
     return Ember.String.pluralize(dasherized);
   }
 });
