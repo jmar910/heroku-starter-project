94a3dcead57c991aea5f637ead3422aef1c099ea
James Martinez <jmar910@gmail.com>
2015-08-13 12:47:48 -0400

    Add state attr for addon service model

--- app/models/addon-service.js
+++ app/models/addon-service.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   humanName: DS.attr('string'),
+  state: DS.attr('string'),
+  
   plans: DS.hasMany('plan', { async: true })
 });
