605fbdd61a2151a4dcc64c62f0add5729ea04fe7
James Martinez <jmar910@gmail.com>
2015-08-13 18:52:37 -0400

    Controller tests for addon-services ctrl

--- tests/unit/controllers/addon-services-test.js
+++ tests/unit/controllers/addon-services-test.js
@@ -6,7 +6,29 @@ moduleFor('controller:addon-services', {
 });
 
 // Replace this with your real tests.
-test('it exists', function(assert) {
-  var controller = this.subject();
-  assert.ok(controller);
+test('filterModels action sets filterType property', function(assert) {
+  assert.expect(2);
+  let ctrl = this.subject();
+
+  assert.equal(ctrl.get('filterType'), 'all');
+
+  ctrl.send('filterModels', 'beta');
+
+  assert.equal(ctrl.get('filterType'), 'beta');
+});
+
+test('_searchFilter returns text filtered models', function(assert) {
+  let ctrl = this.subject();
+  let filepicker = Ember.Object.create({ humanName: 'Filepicker'});
+  let newrelic = Ember.Object.create( { humanName: 'New Relic'} );
+  let postgres = Ember.Object.create( { humanName: 'PostgreSQL'});
+
+  let model = [ filepicker, newrelic, postgres ];
+
+  ctrl.set('model', model);
+  ctrl.set('filterText', 'file');
+
+  let searchFilteredModels = ctrl._searchFilter();
+
+  assert.deepEqual(searchFilteredModels, [ filepicker ]);
 });
