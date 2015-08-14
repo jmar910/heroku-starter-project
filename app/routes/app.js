2d78553c0ab13e90b75284018d84779bfe753057
James Martinez <jmar910@gmail.com>
2015-08-14 16:49:42 -0400

    Final commit, showing available add ons to add.

--- app/routes/app.js
+++ app/routes/app.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from '../mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  afterModel() {
+    let addonServices = this.store.findAll('addon-service');
+    let controller = this.controllerFor('app');
+    controller.set('addonServices', addonServices);
+  }
 });
